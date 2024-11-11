68a87c2ac29a4eecbcc369f5e6855ca62c467607
Chu I-Ting <itingchu1005@gmail.com>
2024-11-11 16:48:58 +0800

    hw2: add error handling

--- HW2/lobby_server.py
+++ HW2/lobby_server.py
@@ -431,7 +431,7 @@ def handle_client(conn, addr):
             if logined:
                 conn.send("\nChoose a option to do: \n1. List rooms\n2. Create room\n3. Join room\n4. Show invitations\n5. Logout\n6. Exit\nEnter: ".encode())
                 option = conn.recv(1024).decode().strip()
-                if option.isspace() or not option.isdigit() or int(option) < 1 or int(option) > 6:
+                if option != "" or not option.isdigit() or int(option) < 1 or int(option) > 6:
                     command = "invalid"
                 else:
                     option = int(option)
@@ -439,7 +439,7 @@ def handle_client(conn, addr):
             else:
                 conn.send("\nChoose a option to do: \n1. Register\n2. Login\n3. Exit\nEnter: ".encode())
                 option = conn.recv(1024).decode().strip()
-                if option.isspace() or not option.isdigit() or int(option) < 1 or int(option) > 3:
+                if option != "" or not option.isdigit() or int(option) < 1 or int(option) > 3:
                     command = "invalid"
                 else:
                     option = int(option)
